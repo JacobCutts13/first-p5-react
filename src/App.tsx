151d5755f77cf5187aae0e2a5c328f1660d27f69
Jc <Jc@email.com>
2022-04-05 17:42:08 +0100

    install p5 turned of ts initialise variable req create template app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import { greet } from "./utils/greet";
+import React from "react";
+import p5 from "p5";
 
-function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+interface IProps {
+  name: string;
+}
+class App extends React.Component {
+  private myRef: React.RefObject<HTMLInputElement>;
+  private myP5: p5;
+
+  constructor(props: IProps) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  Sketch = (p: p5): void => {
+    p.setup = () => {
+      p.background("white");
+    };
+
+    p.draw = () => {
+      p.square(0, 0, 50);
+    };
+  };
+
+  componentDidMount(): void {
+    const node: HTMLElement | undefined =
+      this.myRef.current === null ? undefined : this.myRef.current;
+    this.myP5 = new p5(this.Sketch, node);
+  }
+
+  render(): JSX.Element {
+    return <div ref={this.myRef}></div>;
+  }
 }
 
 export default App;
